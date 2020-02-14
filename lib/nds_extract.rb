c349e95a15dd63d294db57ac462f22e432cd1cbc
concurrent-server-0630 <andycruz008@gmail.com>
2020-02-14 17:22:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,8 +48,8 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
 
- hashnew = directors_totals(source)
- list_of_directors(hashnew)
+directors_totals(source)
+
  
  end
 
